7ab8cb01209a2f233598216791625c514b7c4d4a
Kenil <kenil201446@gmail.com>
2020-05-03 02:34:38 +0530

    Updates

---
+++ precache-manifest.466688138b872c441e970cf73ab8e9ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1e5187e317e6ccf2844872061f1091",
+    "url": "/Monsters-Space/index.html"
+  },
+  {
+    "revision": "22701448318d7b7403e0",
+    "url": "/Monsters-Space/static/css/main.cb959542.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/Monsters-Space/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Space/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22701448318d7b7403e0",
+    "url": "/Monsters-Space/static/js/main.5f9cb6e6.chunk.js"
+  },
+  {
+    "revision": "24b51c1c2539947d5a1f",
+    "url": "/Monsters-Space/static/js/runtime-main.84257ab7.js"
+  }
+]);
